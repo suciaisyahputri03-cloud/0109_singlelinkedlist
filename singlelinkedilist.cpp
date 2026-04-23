5dc0b7eb648357e6a4ca906a95e23e2094f36861
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-23 12:33:17 +0700

    menambahkan output  data

--- singlelinkedilist.cpp
+++ singlelinkedilist.cpp
@@ -58,5 +58,77 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+bool listEmpty()
+    {
+        return (START == NULL);
+    }
 
+    bool search(int nim, node *&previous, node *&current)
+    {
+        previous = START;
+        current - START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delnode(int nim)
+    {
+        node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+        if (current == START)
+            START = START->next;
+        else 
+            previous->next = current->next;
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        { 
+            cout << "\List kosong\n";
+        }
+        else
+        {
+            cout << "\nData di dalam List adalah:\n";
+            node *currentNode = START;
+
+            while (currentNode != NULL)
+            {
+                cout << currentNode-> noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do 
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. menambah data ke dalam list" << endl;
+        cout << "2. mnghapus data dari dalam list" << endl;
+        cout << "3. menmspilkan semua data di dalam list" << endl;
+        cout << "4. mencari data dalam list" << endl;
+        cout << "5. keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
+
+        
     
